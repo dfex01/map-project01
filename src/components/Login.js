9b4e34792e8b8b43d561252170165643e9f9a97b
Alex Rogers <37906277+dfex01@users.noreply.github.com>
2018-10-17 13:12:14 -0400

    login styling/anonymous user button
    added some styling to the log in page. also added a continue anonymous button that allows you to user page anonymously (and logs you out of firebase if you are already logged in)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import firebase from './firebase';
 import firebaseui from 'firebaseui';
+import { Link } from 'react-router-dom';
 
+import '../assets/styles/login.css';
 
 class Login extends Component {
 
@@ -17,11 +19,27 @@ class Login extends Component {
         }
         const ui = new firebaseui.auth.AuthUI(firebase.auth());
         ui.start('#firebaseui-auth-container', uiConfig);
+
       }
 
     render() {
         return (
-            <div id="firebaseui-auth-container"></div>
+            <div className="firebase-outer-container">
+                <div className="auth-welcome">
+                    <h1>Welcome to Map-Project01</h1>
+                    <p>Login to save your markers or continue as an anonymous user</p>
+                </div>
+                <div id="firebaseui-auth-container"></div>
+                <Link to="/main" onClick={firebase.auth().signOut()}>
+                    <button className="anonymous-button">
+                        <img 
+                            src="https://img1.looper.com/img/uploads/2017/06/dumb-and-dumber-780x438_rev1.jpg" 
+                            alt="anonymous-user"
+                            className="firebaseui-idp-icon" />
+                        <span className="firebaseui-idp-text-long">Continue Anonymously</span>
+                    </button>
+                </Link>
+            </div>
         );
     }
 }
